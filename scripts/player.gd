8a1f5fcecbc05fdd63a0c8c0c47a670e4381df00
cloverian <oschlava1000@gmail.com>
2025-10-17 19:37:08 +0200

    Player placeholder sprite added but not implemented.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
 const SPEED : float = 210.0
-const JUMP_VELOCITY : float = -220.0
-const GRAVITY : Vector2 = Vector2(1.0, 300.0)
-var hasDoubleJumped : bool = false
+const JUMP_VELOCITY : float = -300.0
+const GRAVITY : Vector2 = Vector2(0.0, 500.0)
+var jumps : int = 0
 
 func _physics_process(delta: float) -> void:
-	if is_on_floor(): hasDoubleJumped = false
 	if not is_on_floor():
 		velocity += GRAVITY * delta
+		
+	if is_on_floor(): jumps = 0
 	if Input.is_action_just_pressed("Jump"):
-		if is_on_floor(): 
-			velocity.y = JUMP_VELOCITY
-		else:
-			if not hasDoubleJumped:
+		match jumps:
+			0:
+				jumps = 1
+				velocity.y = JUMP_VELOCITY
+			1:
+				jumps = 2
 				velocity.y = JUMP_VELOCITY * 1.2
 			
 	var direction := Input.get_axis("A", "D")
