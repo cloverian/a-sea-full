7e3a58b9002344130fb506fec1368e95f5fad5f0
cloverian <oschlava1000@gmail.com>
2025-10-17 19:04:15 +0200

    (not) fixed double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,18 +6,16 @@ const GRAVITY : Vector2 = Vector2(1.0, 300.0)
 var hasDoubleJumped : bool = false
 
 func _physics_process(delta: float) -> void:
+	if is_on_floor(): hasDoubleJumped = false
 	if not is_on_floor():
 		velocity += GRAVITY * delta
-
 	if Input.is_action_just_pressed("Jump"):
 		if is_on_floor(): 
 			velocity.y = JUMP_VELOCITY
-			hasDoubleJumped = false
-		elif not is_on_floor() and not hasDoubleJumped:
-			velocity.y = JUMP_VELOCITY * 1.2
-			hasDoubleJumped = true
+		else:
+			if not hasDoubleJumped:
+				velocity.y = JUMP_VELOCITY * 1.2
 			
-
 	var direction := Input.get_axis("A", "D")
 	if direction:
 		velocity.x = direction * SPEED
