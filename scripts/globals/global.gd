7c6e985a6ea8913b3fca71d8af7b19aae5f14fba
cloverian <oschlava1000@gmail.com>
2025-10-17 14:40:07 +0200

    More progress, map being worked on

--- scripts/globals/global.gd
+++ scripts/globals/global.gd
@@ -1,8 +1,14 @@
 extends Node
 
+var isGameFullScreen : bool = false
 
 func _ready() -> void:
-	pass
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func _process(_delta) -> void:
-	pass
+	if Input.is_action_just_pressed("F4"):
+		if not isGameFullScreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_size(Vector2i(640, 480))
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,22 +6,22 @@ const GRAVITY : Vector2 = Vector2(1.0, 300.0)
 var hasDoubleJumped : bool = false
 
 func _physics_process(delta: float) -> void:
-    if not is_on_floor():
-        velocity += GRAVITY * delta
+	if not is_on_floor():
+		velocity += GRAVITY * delta
 
-    if Input.is_action_just_pressed("Jump"):
-        if is_on_floor(): 
-            velocity.y = JUMP_VELOCITY
-            hasDoubleJumped = false
-        elif not is_on_floor() and not hasDoubleJumped:
-            velocity.y = JUMP_VELOCITY * 1.2
-            hasDoubleJumped = true
-            
+	if Input.is_action_just_pressed("Jump"):
+		if is_on_floor(): 
+			velocity.y = JUMP_VELOCITY
+			hasDoubleJumped = false
+		elif not is_on_floor() and not hasDoubleJumped:
+			velocity.y = JUMP_VELOCITY * 1.2
+			hasDoubleJumped = true
+			
 
-    var direction := Input.get_axis("A", "D")
-    if direction:
-        velocity.x = direction * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction := Input.get_axis("A", "D")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-    move_and_slide()
+	move_and_slide()
