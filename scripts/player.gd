31b27ca8ddf1b86ac157feb40abc502fa551ca31
cloverian <oschlava1000@gmail.com>
2025-10-17 20:06:02 +0200

    Player placeholder sprite implemented. Also added immediate falling

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const SPEED : float = 210.0
 const JUMP_VELOCITY : float = -300.0
-const GRAVITY : Vector2 = Vector2(0.0, 500.0)
+const GRAVITY : Vector2 = Vector2(0.0, 981.0)
 var jumps : int = 0
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +11,19 @@ func _physics_process(delta: float) -> void:
 		
 	if is_on_floor(): jumps = 0
 	if Input.is_action_just_pressed("Jump"):
-		match jumps:
-			0:
-				jumps = 1
-				velocity.y = JUMP_VELOCITY
-			1:
-				jumps = 2
-				velocity.y = JUMP_VELOCITY * 1.2
+		if jumps == 0:
+			jumps = 1
+			velocity.y = JUMP_VELOCITY
+		elif jumps == 1:
+			jumps = 2
+			velocity.y = JUMP_VELOCITY * 1.2
+	if Input.is_action_just_pressed("S"):
+		velocity.y = -JUMP_VELOCITY * 2
 			
 	var direction := Input.get_axis("A", "D")
+	if direction == -1: $Sprite.flip_h = true
+	elif direction == 1: $Sprite.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
