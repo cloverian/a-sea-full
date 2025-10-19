fb86d850f37b36ae8fb780f5cdc79b9e7e8f9b04
cloverian <oschlava1000@gmail.com>
2025-10-19 19:39:09 +0200

    FIXED FULLSCREEN!!! Still working on initial scene

--- scenes/initialscene.gd
+++ scenes/initialscene.gd
@@ -1,16 +1,28 @@
 extends Node2D
 
-var stage : int = 0
+var stage : int = -1
 
 func _ready() -> void:
 	$Background.visible = false
-	$Text.visible = true
-	$Text.modulate.a = 0
+	$AGameBy.visible = true
+	$AGameBy.modulate.a = 0
+	
+	if Global.hasPlayedGameBefore():
+		gameByToLearnEnglish()
+		get_tree().change_scene_to_file("res://scenes/test.tscn")
+	else: stage = 0
 	
 func _process(_delta: float) -> void:
 	match stage:
-		0: await gameBy()
-		1: await 
+		0: gameByToLearnEnglish()
 		
-func doInitialText() -> void:
-	pass
+func gameByToLearnEnglish() -> void:
+	await Global.wait(1.0)
+	if not Global.hasPlayedGameBefore():
+		$AGameBy.text = "Un gioco da[color=yellow] Clover[/color] e[color=yellow] Figs[/color]"
+	else:
+		$AGameBy.text = "A game by[color=yellow] Clover[/color] and[color=yellow] Figs[/color]"
+	Global.tween($AGameBy, "modulate", Color(1, 1, 1, 1), 1.5)	
+	await Global.wait(2.0)
+	Global.tween($AGameBy, "modulate", Color(1, 1, 1, 0), 1.5)
+	stage += 1
--- scripts/globals/global.gd
+++ scripts/globals/global.gd
@@ -4,16 +4,22 @@ var isGameFullScreen : bool = false
 
 func _ready() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	DisplayServer.window_set_size(Vector2i(640, 480))
 
 func _process(_delta) -> void:
 	if Input.is_action_just_pressed("F4"):
 		if not isGameFullScreen:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			isGameFullScreen = true
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_size(Vector2i(640, 480))
+			isGameFullScreen = false
 	if Input.is_action_just_pressed("ESC"):
 		get_tree().quit(0)
+		
+func hasPlayedGameBefore() -> bool:
+	return FileAccess.file_exists("user://save.sav")
 
 func startDialogue(lines: Array[String]):
 	if get_parent().get_node("WorldCamera"): return
@@ -26,4 +32,7 @@ func wait(amount: float) -> void:
 	await get_tree().create_timer(amount).timeout
 	
 func tween(object: Object, property: NodePath, final_val, duration: float, transition: Tween.TransitionType = Tween.TRANS_LINEAR) -> void:
-	get_tree().create_tween().tween_property(object, property, final_val, duration).set_trans(transition)
+	if transition == Tween.TRANS_LINEAR:
+		get_tree().create_tween().tween_property(object, property, final_val, duration)
+	else:
+		get_tree().create_tween().tween_property(object, property, final_val, duration).set_trans(transition)
